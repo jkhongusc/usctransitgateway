3b3ca882edb3fcccee5899297ee38d87a07a37fd
EC2 Default User <ec2-user@ip-10-254-0-225.us-west-2.aws.usc.edu>
2019-03-11 14:58:56 +0000

    added new env variables

--- monitor/monitortgwresourceshares/commandline.py
+++ monitor/monitortgwresourceshares/commandline.py
@@ -1,9 +1,16 @@
 from main import lambda_handler
+import os
 
 # hard code input to lambda function
 event ={}
 event['REGION'] = 'us-west-2'
 event['TRANSITGATEWAY'] = [ 'tgw-0140bb4dfd328a656' ]
+event['RAMSHARENAME'] = [ 'its-tgw-auto-us-west-2' ]
+
+#os.environ['SLACKTOKEN'] = ''
+os.environ['SLACKNAME'] = 'aws_notifications'
+os.environ['SLACKCHANNEL'] = 'uschomepageadmin'
+
 
 lambda_handler(event,None)
 
