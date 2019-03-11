3d7347ced8a4d89ec86c5d9b141a968e5370822a
EC2 Default User <ec2-user@ip-10-254-0-225.us-west-2.aws.usc.edu>
2019-03-11 06:28:54 +0000

    refining checking logics - still in development

--- monitor/monitortgwresourceshares/main.py
+++ monitor/monitortgwresourceshares/main.py
@@ -20,6 +20,7 @@ Example:
 
 from __future__ import print_function
 
+import os
 import boto3
 
 from dynamodb import TgwDynamoDb
@@ -36,21 +37,24 @@ def lambda_handler(event, context):
     # check to make sure all required parameters are defined
     #if 'SLACKTOKEN' not in os.environ:
     #    raise Exception('SLACKTOKEN environment variable not set')
-    #if 'SLACKNAME' not in os.environ:
-    #    raise Exception('SLACKNAME environment variable not set')
-    #if 'SLACKCHANNEL' not in os.environ:
-    #    raise Exception('SLACKCHANNEL environment variable not set')
+    if 'SLACKNAME' not in os.environ:
+        raise Exception('SLACKNAME environment variable not set')
+    if 'SLACKCHANNEL' not in os.environ:
+        raise Exception('SLACKCHANNEL environment variable not set')
 
     if 'REGION' not in event:
         raise Exception('REGION event variable not set')
     if 'TRANSITGATEWAY' not in event:
         raise Exception('TRANSITGATEWAY event variable not set')
+    if 'RAMSHARENAME' not in event:
+        raise Exception('RAMSHARENAME event variable not set')
 
     #stoken=os.environ['SLACKTOKEN']
-    #sname=os.environ['SLACKNAME']
-    #schannel=os.environ['SLACKCHANNEL']
+    sname=os.environ['SLACKNAME']
+    schannel=os.environ['SLACKCHANNEL']
     region=event['REGION']
     tgws=event['TRANSITGATEWAY']
+    ramsharenames=event['RAMSHARENAME']
 
     #slack_client = SlackClient(stoken)
     #slack_client.api_call( "chat.postMessage", channel=schannel, text="test from EC2")
@@ -69,21 +73,18 @@ def lambda_handler(event, context):
         vpcattachments = myec2.get_transit_gateway_vpc_attachments()
         print("[EC2] Transit Gateway Attachments (%d) " % (len(vpcattachments)) )
         for vpcattachment in vpcattachments:
-            print("    AWS (%s)" % (vpcattachment['VpcOwnerId']) )
+            print("    AWS (%s) VPC (%s)" % (vpcattachment['VpcOwnerId'],vpcattachment['VpcId']) )
 
         mydynamo = TgwDynamoDb(region)
         tgwlist = mydynamo.get_transit_gateway() 
         vpclist = mydynamo.get_vpc() 
         vpnlist = mydynamo.get_vpn() 
-        #print ("\n")
         print("[DynamoDb] (%d) Transit Gateway, (%d) VPCs, (%d) VPNs" % (len(tgwlist),len(vpclist),len(vpnlist)) )
         for tgwitem in tgwlist:
-            print("Transit Gateway (%s) CIDR (%s) AWS (%s)" % (tgwitem['TransitGatewayId'],tgwitem['Cidr'],tgwitem['Account']) )
-        #print ("\n")
+            print("Transit Gateway (%s) CIDR (%s) AWS (%s) VPC (%s)" % (tgwitem['TransitGatewayId'],tgwitem['Cidr'],tgwitem['Account'],tgwitem['ResourceId']) )
         #print (vpclist)
         for vpcitem in vpclist:
             print("    VPC (%s) CIDR (%s) AWS (%s)" % (vpcitem['ResourceId'],vpcitem['Cidr'],vpcitem['Account']) )
-        #print ("\n")
         #print (vpnlist)
         for vpnitem in vpnlist:
             print("    VPN (%s) CIDR (%s) AWS (%s)" % (vpnitem['ResourceId'],vpnitem['Cidr'],vpnitem['Account']) )
@@ -91,11 +92,34 @@ def lambda_handler(event, context):
 
         # Add comparisons/notification/alerts here
 
+
+        # iterate through DynamoDB VPC list
+        # check if VPC is in Transit Gateway Attachment list, if not create alert
+        # for errors: account admin has to manually check or attach
+        print ("\nNotifications")
+        print ("Check #1: DynamoDB and Transit Gateway Attachments")
+        for vpcitem in vpclist:
+            if not any (d['VpcId'] == vpcitem['ResourceId'] for d in vpcattachments):
+                print("    [ERROR] VPC (%s) CIDR (%s) AWS (%s) is not found in TGW Attachment list" % (vpcitem['ResourceId'],vpcitem['Cidr'],vpcitem['Account']) )
+            else:
+                print("    VPC (%s) CIDR (%s) AWS (%s) is found in TGW Attachment list" % (vpcitem['ResourceId'],vpcitem['Cidr'],vpcitem['Account']) )
+
+
+        # iterate through Transit Gateway Attachment list
+        # check if VPC is in DynamoDB VPC list, if not create alert
+        # for errors: account admin has to manually check
+        print ("Check #2: Transit Gateway Attachments and DynamoDb")
+        for vpcattachment in vpcattachments:
+            if not any (d['ResourceId'] == vpcattachment['VpcId'] for d in vpclist):
+                print("    [ERROR] VPC (%s) AWS (%s) is not found in DynamoDb list" % (vpcattachment['VpcId'],vpcattachment['VpcOwnerId']) )
+            else:
+                print("    VPC (%s) AWS (%s) is found in DynamoDb list" % (vpcattachment['VpcId'],vpcattachment['VpcOwnerId']) )
+
+
         # iterate through DynamoDB VPC list, ignore VPCs from master account
         # check if VPC is in RAM principal list, if not create alert
-	# for errors: should automatically create new resource share with principal
-        print ("\nNotifications")
-        print ("Check #1: DynamoDB and RAM")
+	# for errors: automatically create new resource share with principal (should check invitations first)
+        print ("Check #3: DynamoDB and RAM")
         for vpcitem in vpclist:
             if (vpcitem['Account'] == tgwitem['Account']):
                 print("    VPC (%s) CIDR (%s) AWS (%s) is found in master account" % (vpcitem['ResourceId'],vpcitem['Cidr'],vpcitem['Account']) )
@@ -103,23 +127,19 @@ def lambda_handler(event, context):
             #if not any (d['Account'] == vpcitem['Account'] for d in vpclist):
             if not any (d['id'] == vpcitem['Account'] for d in ramprincipals):
                 print("    [ERROR] VPC (%s) CIDR (%s) AWS (%s) is not found in RAM list" % (vpcitem['ResourceId'],vpcitem['Cidr'],vpcitem['Account']) )
-	        # TBD - for errors: should automatically create new resource share with principal
+                print ("        sending RAM resource share to: " + vpcitem['Account'])
+                #print ("%s arn:aws:ec2:%s:%s:transit-gateway/%s %s," %( ramsharenames[0], region, tgwitem['Account'], tgwitem['TransitGatewayId'], vpcitem['Account'] ))
+	        # TBD - for errors: automatically create new resource share with principal
+                response = myram.create_resource_share( ramsharenames[0],  ['arn:aws:ec2:'+region+':'+tgwitem['Account']+':transit-gateway/'+tgwitem['TransitGatewayId'] ],[ vpcitem['Account']])
+                #print (response)
+
             else:
                 print("    VPC (%s) CIDR (%s) AWS (%s) is found in RAM list" % (vpcitem['ResourceId'],vpcitem['Cidr'],vpcitem['Account']) )
 
 
-        # iterate through DynamoDB VPC list, ignore VPCs from master account
-        # check if VPC is in Transit Gateway Attachment list, if not create alert
-        # for errors: account admin has to manually re-attach
-        print ("Check #2: DynamoDB and Transit Gateway Attachments")
-        for vpcitem in vpclist:
-            if (vpcitem['Account'] == tgwitem['Account']):
-                print("    VPC (%s) CIDR (%s) AWS (%s) is found in master account" % (vpcitem['ResourceId'],vpcitem['Cidr'],vpcitem['Account']) )
-                continue
-            if not any (d['VpcOwnerId'] == vpcitem['Account'] for d in vpcattachments):
-                print("    [ERROR] VPC (%s) CIDR (%s) AWS (%s) is not found in TGW Attachment list" % (vpcitem['ResourceId'],vpcitem['Cidr'],vpcitem['Account']) )
-            else:
-                print("    VPC (%s) CIDR (%s) AWS (%s) is found in TGW Attachment list" % (vpcitem['ResourceId'],vpcitem['Cidr'],vpcitem['Account']) )
+
+
+
 
 
         '''
